5e20daca1a873e8b94ba97bcc01a9a86d1784dde
Rebeca Baruch <rebecabaruch@outlook.com>
2025-08-13 19:13:00 -0300

    fix: unnecessary imports

--- src/app/landing-page/page.tsx
+++ src/app/landing-page/page.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import LandingPageController from './controller/index.controller';
 
 export default LandingPageController;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import LandingPageController from "./landing-page/page";
 
 export default function Home() {
