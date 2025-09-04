5205d0b4a49e880e5a0ef282f97ccc7537488292
Rebeca Baruch <rebecabaruch@outlook.com>
2025-09-04 03:42:55 -0300

    card spec service component

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,27 @@ module.exports = {
           light: "#DAF0FF",
           lightest: "#FBFDFF",
         },
+        green: {
+          DEFAULT: "#38e977",
+          dark: "#144827",
+          mid: "#38e9768f",
+        },
+        gray: {
+          DEFAULT: "#838383",
+          darkest: "2c2c2c",
+          dark: "#4c4c4c",
+          light: "#c7c7c7",
+          lightest: "#e5e5e5",
+          w: "#f0f0f0",
+        },
+        black: "#0f1417",
+        white: "#fdfdfdff",
+
+        yellow: "#fdb94b",
+        red: "#ff4d4f",
+        boxShadow: {
+          "md-gray-light": "#c7c7c7",
+        },
       },
     },
   },
