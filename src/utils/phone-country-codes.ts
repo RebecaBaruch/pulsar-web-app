ac3e84dece574d4db6600a54f5710fa0748e53f0
Rebeca Baruch <rebecabaruch@outlook.com>
2025-11-13 14:59:00 -0300

    feat: file within the country codes data

---
+++ src/utils/phone-country-codes.ts
@@ -0,0 +1,14 @@
+export const phoneCountryCodes = [
+  { code: "BR", value: "+55", name: "Brasil" },
+  { code: "US", value: "+1", name: "Estados Unidos" },
+  { code: "GB", value: "+44", name: "Reino Unido" },
+  { code: "PT", value: "+351", name: "Portugal" },
+  { code: "JP", value: "+81", name: "Japão" },
+  { code: "DE", value: "+49", name: "Alemanha" },
+  { code: "FR", value: "+33", name: "França" },
+  { code: "IT", value: "+39", name: "Itália" },
+  { code: "ES", value: "+34", name: "Espanha" },
+  { code: "MX", value: "+52", name: "México" },
+  { code: "IN", value: "+91", name: "Índia" },
+  { code: "RU", value: "+7", name: "Rússia" },
+];
