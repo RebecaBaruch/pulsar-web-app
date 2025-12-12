56866386731afa2d955c4ab53c5aa736cda28e8e
Rebeca Baruch <rebecabaruch@outlook.com>
2025-12-12 11:02:46 -0300

    fix: login form responsive layout

--- src/app/login/form/view/index.view.tsx
+++ src/app/login/form/view/index.view.tsx
@@ -24,50 +24,77 @@ export default function LoginFormView({
   isDisabled,
 }: Props) {
   return (
-    <div className="flex flex-col w-full md:w-3/4 lg:w-fit gap-15 md:gap-8 lg:gap-10 xl:gap-12">
-      <img
-        src="/images/horizontal-logo.png"
-        alt="Logo da Pulsar"
-        className="lg:w-[150px] w-[150px]"
-      />
+    <section className="flex w-screen min-h-screen justify-center lg:items-center pt-20 lg:p-4">
+      <div className="flex flex-col w-full p-6 md:p-15 lg:py-10 lg:px-6 lg:max-w-lg gap-15 md:gap-8 lg:gap-10 lg:bg-white md:rounded-xl lg:shadow-md">
+        <img
+          src="/images/horizontal-logo.png"
+          alt="Logo da Pulsar"
+          className="lg:w-[150px] w-[150px]"
+        />
 
-      <div className="flex flex-col gap-2">
-        <h1 className="text-2xl md:text-3xl lg:text-2xl font-bold text-black">
-          Bem-vindo de volta!
-        </h1>
-        <p className="text-lg md:text-md lg:text-md text-gray-darkest">
-          Acesse sua conta para continuar cuidando do que realmente importa:
-          você.
-        </p>
+        <div className="flex flex-col gap-2">
+          <h1 className="text-2xl md:text-3xl lg:text-2xl font-bold text-black">
+            Bem-vindo de volta!
+          </h1>
+          <p className="text-lg md:text-md lg:text-md text-gray-darkest">
+            Acesse sua conta para continuar cuidando do que realmente importa:
+            você.
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-6">
+          <form
+            onSubmit={onSubmit}
+            className="flex flex-col items-center gap-6"
+          >
+            <div className="flex flex-col gap-6 w-full">
+              <InputField
+                label="CPF, e-mail ou telefone"
+                type="text"
+                placeholder="Digite seu usuário"
+                value={username}
+                onChange={(value) => onUsernameChange(value)}
+                required
+                errorMessage={errors.username}
+                error={!!errors.username}
+              />
+
+              <div className="flex flex-col gap-3 items-end">
+                <InputField
+                  className="w-full"
+                  label="Senha"
+                  type="password"
+                  placeholder="Digite a sua senha"
+                  value={password}
+                  onChange={(value) => onPasswordChange(value)}
+                  required
+                  errorMessage={errors.password}
+                  error={!!errors.password}
+                />
+
+                <a
+                  href="/"
+                  className="text-blue lg:text-xs font-semibold underline"
+                >
+                  Esqueci minha senha
+                </a>
+              </div>
+            </div>
+
+            <PrimaryButton
+              type="submit"
+              text="Próximo"
+              isDisabled={isDisabled}
+            />
+            <span className="text-center lg:text-base 2xl:text text-lg">
+              Ainda não possui uma conta?{" "}
+              <a href="/login" className="text-blue font-semibold underline">
+                Cadastre-se
+              </a>
+            </span>
+          </form>
+        </div>
       </div>
-
-      <div className="flex flex-col gap-6">
-        <form onSubmit={onSubmit} className="flex flex-col gap-6">
-          <InputField
-            label="CPF, e-mail ou telefone"
-            type="text"
-            placeholder="Digite seu usuário"
-            value={username}
-            onChange={(value) => onUsernameChange(value)}
-            required
-            errorMessage={errors.username}
-            error={!!errors.username}
-          />
-
-          <InputField
-            label="Senha"
-            type="password"
-            placeholder="Digite a sua senha"
-            value={password}
-            onChange={(value) => onPasswordChange(value)}
-            required
-            errorMessage={errors.password}
-            error={!!errors.password}
-          />
-
-          <PrimaryButton type="submit" text="Próximo" isDisabled={isDisabled} />
-        </form>
-      </div>
-    </div>
+    </section>
   );
 }
