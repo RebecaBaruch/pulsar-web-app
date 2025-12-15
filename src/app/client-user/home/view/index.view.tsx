e7d00f2bec05faed792dbb34cb333faf261622dd
Rebeca Baruch <rebecabaruch@outlook.com>
2025-12-15 13:56:27 -0300

    feat: responsive client homepage layout

--- src/app/client-user/home/view/index.view.tsx
+++ src/app/client-user/home/view/index.view.tsx
@@ -1,11 +1,23 @@
 "use client";
 
 import React from "react";
+import NextSessionHeader from "../components/NextSessionHeader";
+import MySessions from "../components/MySessions";
+import SpecialistsSection from "../components/SpecialistsSection";
 
 export default function HomeView() {
   return (
-    <div className="flex flex-col w-full md:w-3/4 lg:w-fit gap-15 md:gap-8 lg:gap-10 xl:gap-12">
-      Home
-    </div>
+    <section className="flex w-screen overflow-x-hidden min-h-screen justify-center lg:items-center pt-20 lg:p-4 pb-10">
+      <div className="flex flex-col gap-20 w-full p-6 md:p-15">
+        <div className="flex flex-col gap-10 w-full">
+          <span>
+            <h1 className="text-5xl text-black font-bold"> Olá, Ana 👋 </h1>
+          </span>
+          <NextSessionHeader />
+          <MySessions />
+        </div>
+        <SpecialistsSection />
+      </div>
+    </section>
   );
 }
