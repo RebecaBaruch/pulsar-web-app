588a1564e7c1b8029aa2f00b1e549d016cb1aca8
Rebeca Baruch <rebecabaruch@outlook.com>
2026-08-10 22:40:07 -0300

    fix: re-index contact-whatsapp export and update professional code mask

--- src/utils/contact-whatsapp.ts
+++ src/utils/contact-whatsapp.ts
@@ -1,4 +1,4 @@
-export const contactWahtsappUtil = (phoneNumber: string, message: string) => {
+export const contactWhatsappUtil = (phoneNumber: string, message: string) => {
   const formattedPhoneNumber = phoneNumber.replace(/\D/g, '');
   const whatsappUrl = `https://wa.me/${formattedPhoneNumber}?text=${encodeURIComponent(message)}`;
   window.open(whatsappUrl, "_blank");
